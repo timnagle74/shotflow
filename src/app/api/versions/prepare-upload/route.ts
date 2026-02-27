aa979ef35cd5f2e0a1dd4dbb8291b7bb7e1663ca
Stu <stu@creativeintegrations.com>
2026-02-27 13:20:27 -0600

    fix: add useProxy to type definition

--- src/app/api/versions/prepare-upload/route.ts
+++ src/app/api/versions/prepare-upload/route.ts
@@ -78,6 +78,7 @@ export async function POST(request: NextRequest) {
       storageUpload?: {
         url: string;
         path: string;
+        useProxy?: boolean;
       };
       streamUpload?: {
         libraryId: string;
